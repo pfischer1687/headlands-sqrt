0b30a01320801f4730479b54f7bf50a4995ae529
pfischer1687 <pfisch789@outlook.com>
2023-05-10 14:09:04 -0700

    Adds the different methods for calculating the square root to the main function in src/headlands.cpp

--- src/headlands.cpp
+++ src/headlands.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-    std::cout << "sqrt(42) = " << square_root(42) << "\n";
+    std::cout << "square_root_stl(42) = " << square_root_stl(42) << "\n";
+    std::cout << "square_root_pow(42) = " << square_root_pow(42) << "\n";
+    std::cout << "square_root_log(42) = " << square_root_log(42) << "\n";
+    std::cout << "square_root_log10(42) = " << square_root_log10(42) << "\n";
+    std::cout << "square_root_newton(42) = " << square_root_newton(42) << "\n";
     return 0;
 }
