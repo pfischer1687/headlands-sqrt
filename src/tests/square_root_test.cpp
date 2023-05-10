8925cea771704397b775fe4a9d75b1a0ed40f939
pfischer1687 <pfisch789@outlook.com>
2023-05-10 00:27:24 -0700

    Adds negative int to tests

--- src/tests/square_root_test.cpp
+++ src/tests/square_root_test.cpp
@@ -1,7 +1,7 @@
 #include "../libs/square_root.h"
 #include "UnitTest.h"
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 bool summary_flag = true;
 
@@ -14,11 +14,12 @@ int main()
 
     std::cout << "\nTesting square_root -----\n";
 
-    u.test("positive int", square_root((int)42) == sqrt(42));
-    u.test("positive float", square_root((float)42) == sqrt(42));
-    u.test("positive double", square_root((double)42) == sqrt(42));
-    u.test("positive long double", square_root((long double)42) == sqrt(42));
+    u.test("positive int", square_root((int)42) == sqrt((int)42));
+    u.test("positive float", square_root((float)42.1) == sqrt((float)42.1));
+    u.test("positive double", square_root((double)42.2) == sqrt((double)42.2));
+    u.test("positive long double", square_root((long double)42.3) == sqrt((long double)42.3));
     u.test("zero", square_root(0) == sqrt(0));
+    u.test("negative int", std::isnan(square_root(-42)));
 
     u.report();
 
